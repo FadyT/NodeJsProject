c8145e333eeea49afcab0e6fe953b47fe08417ac
fady <fady.tarek.1995@gmail.com>
2024-07-23 16:30:57 +0300

    Fixed : Refine Your API: Missing Image Caching

--- src/routes/api/Resizer.ts
+++ src/routes/api/Resizer.ts
@@ -6,22 +6,6 @@ import { bool } from "sharp";
 const resizer = Express.Router();
 const sharp = require("sharp");
 
-
-function checkFileExist (path: String): boolean{
-  
-      // Check if the file exists in the current directory.
-      const file = `${process.cwd()}${path}`;
-      access(file, constants.F_OK, (check) => {
-        if(check != null){
-          return false;
-        }else{
-          return true;
-        }
-      });
-      return false;
-
-}
-
 async function resizeImage(w : Number ,  h : Number, picName : String , res :any) {
 
     
@@ -35,12 +19,16 @@ async function resizeImage(w : Number ,  h : Number, picName : String , res :any
         width: w,
         height: h
       })
-      .toFile(`./src/images/resized/${picName}-resized.png`);
+      .toFile(`./src/images/resized/${picName}-resized-${w}X${h}.png`);
       console.log("resized image saved !");
-      res.sendFile(process.cwd() +`/src/images/resized/${picName}-resized.png`);
-  } catch (error) {
-    //console.log(error);
+      res.sendFile(process.cwd() +`/src/images/resized/${picName}-resized-${w}X${h}.png`);
+  } 
+  
+  catch (error) {
     if(error == "Error: missing data"){
+    console.log("Can't generate image ");
+    console.log("Data is missing !");
+
     res.send("<p>please enter image name , width & height </p>");
     }else{
       res.send("<p>File Not Found please check the name </p>");
@@ -51,26 +39,23 @@ async function resizeImage(w : Number ,  h : Number, picName : String , res :any
 
 
 resizer.get('/' , (req , res ) =>{
-      let fileExist:boolean = false;
 
-      // Check if the file exists in the current directory.
-      if(checkFileExist(`${process.cwd()}/src/images/resized/${req.query.name}-resized.png`)){
-        console.log("file already exists ");
-        res.sendFile(process.cwd() +`/src/images/resized/${req.query.name}-resized.png`);
-      }else{
-        console.log("resized file not found ");
-        resizeImage(Number(req.query.width) ,Number( req.query.height ) ,String( req.query.name) , res);
-      }
+      // resized image file full path
+      const file = `${process.cwd()}/src/images/resized/${req.query.name}-resized-${req.query.width}X${req.query.height}.png`;
+      console.log(`looking for image at ${file}`);
+      access(file, constants.F_OK, (check) => {
+        if(check == null){
+          console.log("Image found ...");
+          console.log("loading it from memory ");
+          res.sendFile(process.cwd() +`/src/images/resized/${req.query.name}-resized-${req.query.width}X${req.query.height}.png`);
+        }else{
+          console.log("Image not found ...");
+          console.log("Generating image ");
+          resizeImage(Number(req.query.width) ,Number( req.query.height ) ,String( req.query.name) , res);
+        }
+      });
 
-
-      
-  console.log(`req is ${req.url} + name is ${req.query.name} + width : ${req.query.width} + height : ${req.query.height} `);
-  var fullUrl = req.protocol + '://' + req.get('host') ;//+ req.originalUrl;
-  console.log(fullUrl +"" + process.cwd() +'/src/images/resized/sammy-resized.png');
-  //res.send(`<img src="${fullUrl}/src/images/resized/${req.query.name}-resized.jpg" alt="${req.query.name}">`);
-  //res.send(`<img src="./src/images/resized/sammy-resized.png" alt="${req.query.name}">`);
-  //res.sendFile(process.cwd() +'/src/images/sammy-resized.png');
-
+  return res.statusCode;
 })
 
 export default resizer;
