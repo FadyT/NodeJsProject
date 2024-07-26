9e64002eb89ef9d0ccfc42d08423ffff6341fc7f
fady <fady.tarek.1995@gmail.com>
2024-07-26 09:46:45 +0300

    added super test to the project

--- src/tests/resizerSpec.ts
+++ src/tests/resizerSpec.ts
@@ -1,6 +1,12 @@
 const request = require('request');
 const server = require('../index');
 
+// supertest
+import req from 'supertest';
+const express = require('express');
+const app = express();
+
+
     it('should return 200 response code', function () {
         request.get('http://localhost:3000/api/resizer?name=sammy&width=500&height=250', function (error: any, response: { statusCode: any; }) {
             expect(response.statusCode).toEqual(200);
@@ -13,3 +19,20 @@ const server = require('../index');
             expect(response.statusCode).toEqual(404);
         });
     });
+
+
+    it('should return 200 response code', function () {
+        expect(20).toEqual(20);
+    });
+
+    
+    it('request testing',async function () {
+    req(app)
+    .get('/user')
+    //.expect('Content-Type', /json/)
+    //.expect('Content-Length', '15')
+    .expect(400)
+    .end(function(err, res) {
+      if (err) throw err;
+    });
+});
